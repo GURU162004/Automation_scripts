169b806f16071e40c96065a737102e95ec57e829
GURU162004 <prakashguru1612@gmail.com>
2025-12-04 18:46:44 +0530

    Added scripts to run using git and run a tpch dataset

--- install_postgresql_tcph.py
+++ install_postgresql_tcph.py
@@ -1,14 +1,21 @@
 import subprocess
 import sys
 import os
+import time
+import psycopg2
 
 DIR_NAME = "pgsql_git"
 SOURCE_URL = "https://github.com/postgres/postgres.git"
+TCPH_URL = "https://github.com/gregrahn/tpch-kit.git"
 
 HOME_DIR = os.path.expanduser("~")
 INSTALL_PATH = os.path.join(HOME_DIR, DIR_NAME)
 SOURCE_FOLDER = os.path.join(INSTALL_PATH, "postgres")
-bin_dir = os.path.join(INSTALL_PATH,"bin")
+TCPH_DIR = os.path.join(HOME_DIR, "tcph_kit")
+DATA_DIR = os.path.join(INSTALL_PATH,"data")
+BIN_DIR = os.path.join(INSTALL_PATH,"bin")
+
+tables = ["customer","lineitem","nation","orders","part","partsupp","region","supplier"]
 
 def run(command, cwd=None, shell=True):
     print(f"\n Running: {command}")
@@ -22,10 +29,9 @@ def run(command, cwd=None, shell=True):
 def clone_source():
     if not os.path.exists(INSTALL_PATH):
         os.makedirs(INSTALL_PATH)
-    os.chdir(INSTALL_PATH)
-    if not os.path.exists(SOURCE_FOLDER)
-        print("\n git cloning postgresql repository ...")
+        print("\n git cloning repository ....")
         run(f"git clone {SOURCE_URL}")
+    os.chdir(INSTALL_PATH)
     
     print("\n listing branches : ")
     os.chdir(SOURCE_FOLDER)
@@ -34,12 +40,12 @@ def clone_source():
     run(f"git checkout REL_{VERSION}_STABLE")
           
 def build_postgres():
-    postgres_bin = os.path.join(bin_dir,"postgres")
+    postgres_bin = os.path.join(BIN_DIR,"postgres")
     if os.path.exists(postgres_bin):
         print("\nPostgreSQL is already compiled and installed")
         return
     os.chdir(SOURCE_FOLDER)
-    print("\n Configuring, Compiling and Installing PostgreSQL")
+    print("\n Configuring and Compiling ")
     run(f"./configure --prefix={INSTALL_PATH} --with-pgport=5433")
     run("make")
     run("make install")
@@ -48,21 +54,40 @@ def setup_database():
     print("\n Setting up Database")
     data_dir = os.path.join(INSTALL_PATH,"data")
     if not os.path.exists(data_dir):
-        run(f"{bin_dir}/initdb -D {data_dir}")
+        run(f"{BIN_DIR}/initdb -D {data_dir}")
     status = subprocess.run(f"{bin_dir}/pg_ctl -D {data_dir} status", shell=True)
     if(status.returncode!=0):
         print("\nPostgreSQL is not running. Starting server...")
         run(f"{bin_dir}/pg_ctl -D {data_dir} -l logfile start")
     else:
         print("\nPostgreSQL server is already running.")
-    run(f"{bin_dir}/createdb test")
-    run(f"{bin_dir}/psql test")
+    
+def setup_tcph():
+    if not os.path.exists(TCPH_DIR)
+        os.makedirs(TCPH_DIR)
+        print("\n git cloning repository ....")
+        run(f"git clone {TCPH_URL}")
+    os.chdir(os.path.join(INSTALL_PATH,"dbgen"))
+    run("make clean")
+    run("make MACHINE=LINUX DATABASE=POSTGRESQL")
+    run(f"{BIN_DIR}/createdb -p 5433 tpch")
+    create_schema = "CREATE SCHEMA IF NOT EXISTS tpch;"
+    run(f"{BIN_DIR}/psql -p 5433 -d tpch -c {create_schema}")
+    run(f"{BIN_DIR}/psql -p 5433 -d tpch -f dss.ddl")
+    
+    for tbl in tables:
+        file = f"{tbl}.tbl"
+        sql = f"\copy {tbl} FROM '{file}' WITH (FORMAT csv, DELIMITER '|', NULL '')"
+        run(f"{BIN_DIR}/psql -p 5433 -d tpch -c {sql})
+    
     
 if __name__=="__main__":
     clone_source()
     build_postgres()
     setup_database()
-    print("\n Postgresql installed and configured successfully")
+    setup_tcph()
+    run_queries()
+    print("\n Postgresql installed successfully")
     
     
 
