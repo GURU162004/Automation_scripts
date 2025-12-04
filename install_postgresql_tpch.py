e93e8bb358f46de8b9d3faaeeba9cc7e672910dc
GURU162004 <prakashguru1612@gmail.com>
2025-12-04 22:08:00 +0530

    fixed db cmd quotes error

--- install_postgresql_tpch.py
+++ install_postgresql_tpch.py
@@ -73,8 +73,8 @@ def setup_tpch():
 
     run(f"{BIN_DIR}/dropdb -p 5433 --if-exists tpch")
     run(f"{BIN_DIR}/createdb -p 5433 tpch")
-    create_schema = "CREATE SCHEMA IF NOT EXISTS tpch;"
-    run(f"{BIN_DIR}/psql -p 5433 -d tpch -c {create_schema}")
+    create_schema = "\"CREATE SCHEMA IF NOT EXISTS tpch;\""
+    run(f'{BIN_DIR}/psql -p 5433 -d tpch -c {create_schema}')
     run(f"{BIN_DIR}/psql -p 5433 -d tpch -f dss.ddl")
     
     for tbl in tables:
